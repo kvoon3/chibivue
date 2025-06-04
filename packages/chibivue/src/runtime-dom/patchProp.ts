ef1dc12d3f0ab2a252f0a1f36b937a1ba956f134
kvoon <kvoon@duck.com>
2025-06-04 14:42:59 +0800

    perf: createInvoker

--- packages/chibivue/src/runtime-dom/patchProp.ts
+++ packages/chibivue/src/runtime-dom/patchProp.ts
@@ -1,7 +1,14 @@
 import type { RendererOptions } from '../runtime-core/rendererOpts'
 
 // 1. add all event
-// 2. listen event once by invokers
+// 2. listen event once by bind invoker and patch invoker's value
+
+// NOTE: type: Function & { value: Function }
+interface Invoker extends EventListener {
+  value: EventValue
+}
+
+type EventValue = (e: Event) => void
 
 function addEventListener(
   node: Node,
@@ -23,7 +30,8 @@ export const patchProp: RendererOptions<Node>['patchProp'] = (node, props) => {
   for (const [key, value] of Object.entries(props)) {
     // is event
     if (key.startsWith('on') && typeof value === 'function') {
-      patchEvent(node, key, value)
+      // TODO: type
+      patchEvent(node as Element, key, value)
     }
     // is attr
     else if (typeof value === 'string') {
@@ -34,9 +42,9 @@ export const patchProp: RendererOptions<Node>['patchProp'] = (node, props) => {
 }
 
 function patchEvent(
-  node: Node & { _vei?: Record<string, { value: EventListener } | undefined> },
+  node: Element & { _vei?: Record<string, Invoker | undefined> },
   rawName: string,
-  value?: (evt: Event) => void,
+  value: EventValue | null,
 ): void {
   const invokers = node._vei || (node._vei = {})
   const existingInvoker = invokers[rawName]
@@ -49,10 +57,10 @@ function patchEvent(
     const eventName = parseName(rawName)
     if (value) {
       // add
-      invokers[rawName] = { value }
-      addEventListener(node, eventName, (e) => {
-        invokers[rawName]?.value(e)
-      })
+      const invoker = createInvoker(value)
+
+      invokers[rawName] = invoker
+      addEventListener(node, eventName, invoker)
     }
     else if (existingInvoker) {
       // remove
@@ -64,6 +72,14 @@ function patchEvent(
 
 function parseName(key: string): string {
   return key.slice(2).toLocaleLowerCase()
+}
+
+function createInvoker(value: EventValue): Invoker {
+  const invoker: Invoker = (evt: Event) => {
+    invoker.value(evt)
+  }
+  invoker.value = value
+  return invoker
 }
 
 function patchAttr(node: Element, key: string, value: string): void {
